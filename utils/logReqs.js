1ab73fa28044908d971e0fd4328b11b8378c1818
YoanWai <thebook90yw@gmail.com>
2023-05-06 14:36:42 +0300

    Adjusted logReqs func

--- utils/logReqs.js
+++ utils/logReqs.js
@@ -1,9 +1,11 @@
 function logReqs(req, res, next) {
   console.log(
-    `[${req.method}] ${req.url} - ${new Date().toLocaleString()} - ${
-      req.ip
-    } - ${req.headers["user-agent"]}`
+    `REQ: METHOD: [${req.method}]  PATH: ${
+      req.url
+    } - ${new Date().toLocaleString()} - ${req.ip} - ${res.statusCode}`
   );
+
+  console.log();
   next();
 }
 
