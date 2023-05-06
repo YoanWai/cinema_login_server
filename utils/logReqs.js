0f271f2ef2e77da2d6eb88fdf834b7b4dab201ef
YoanWai <thebook90yw@gmail.com>
2023-05-07 00:41:22 +0300

    logReqs bugfix

--- utils/logReqs.js
+++ utils/logReqs.js
@@ -4,7 +4,7 @@ function logReqs(req, res, next) {
       req.url
     } \n Date: ${new Date().toLocaleString()} \n IP: ${req.ip} \n StatusCode: ${
       res.statusCode
-    } \n StatusMessage: ${res.statusMessage} \n Body: ${JSON.stringify(req)} \n`
+    } \n StatusMessage: ${res.statusMessage} \n Body: ${req} \n`
   );
 
   console.log();
